45fb202fb9abbf07dfe2f493e93fd5bbfd6e0c73
Diego Hernandes <diego@hernandev.com>
2015-09-19 23:15:33 -0300

    classe User adicionada

---
+++ src/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Codecasts;
+
+class User
+{
+    /**
+     * @var string User First Name
+     */
+    protected $firstName;
+
+    /**
+     * @var string User Last Name
+     */
+    protected $lastName;
+
+    /**
+     * @var int User Age
+     */
+    protected $age;
+}
