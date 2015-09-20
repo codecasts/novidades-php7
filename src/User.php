aef278e83a55d4dd69c3906c2411f7b0ce097d68
Diego Hernandes <diego@hernandev.com>
2015-09-19 23:16:51 -0300

    getters e setters

--- src/User.php
+++ src/User.php
@@ -4,18 +4,39 @@ namespace Codecasts;
 
 class User
 {
-    /**
-     * @var string User First Name
-     */
     protected $firstName;
 
-    /**
-     * @var string User Last Name
-     */
     protected $lastName;
 
-    /**
-     * @var int User Age
-     */
     protected $age;
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
 }
